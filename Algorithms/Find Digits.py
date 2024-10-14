78e0f4e485e61e684529a651fd829051badf8ce3
Vince Swu <vinceswu88@gmail.com>
2024-10-14 15:09:54 +0530

    Added file

---
+++ Algorithms/Find Digits.py
@@ -0,0 +1,14 @@
+def is_divisor(n, d):
+    return d != 0 and n % d == 0
+
+n = int(input())
+nl = []
+for i in range(n):
+    num = int(input())
+    nl.append(num)
+
+for num in nl:
+    digits = [int(digit) for digit in str(num)]
+    count = sum(1 for d in digits if is_divisor(num, d))
+    print(count)
+
