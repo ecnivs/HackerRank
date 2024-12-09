1a0b64d745218f88053cd49884f910a050a22586
Vince Swu <vinceswu88@gmail.com>
2024-12-09 15:02:34 +0530

    solved

--- Algorithms/Missing Numbers.py
+++ Algorithms/Missing Numbers.py
@@ -15,16 +15,11 @@ def missingNumbers(arr, brr):
 
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
     n = int(input().strip())
     arr = list(map(int, input().rstrip().split()))
-
     m = int(input().strip())
     brr = list(map(int, input().rstrip().split()))
-    
     result = missingNumbers(arr, brr)
-
     fptr.write(' '.join(map(str, result)))
     fptr.write('\n')
-
     fptr.close()
